5d3f683f29341e8a4411c7d42942da73abd0a07e
sahilbhandare89 <sahilbhandare5@gmail.com>
2025-06-11 12:50:31 +0530

    Create ArrayPrint.java

---
+++ ArrayPrint.java
@@ -0,0 +1,15 @@
+public class Arrayprint {
+
+	public static void main(String[] args) {
+		int i;
+		int[] arr= {1,2,3,4,5};
+		for(i=0;i<=arr.length;i++) 
+		{
+			System.out.println(arr[i]);
+			
+		}
+		
+		
+	}
+
+}
