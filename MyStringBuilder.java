ad7dc9b15604c9c7c610f4c5536187a088994ca2
sahilbhandare89 <sahilbhandare5@gmail.com>
2025-06-12 10:19:03 +0530

    Create MyStringBuilder.java

---
+++ MyStringBuilder.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class MyStringBuilder{
+
+    
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("Tonny");
+        System.out.println(sb.charAt(2));
+        System.out.println(sb.insert(0, 'p'));
+        System.out.println(sb.delete(1, 2));
+        sb.setCharAt(1, p);
+    }
+}
